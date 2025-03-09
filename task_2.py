9d5fad06b9a9dff552b109485ac86bdf697600cb
alex <alex.kudelko@gmail.com>
2025-03-09 18:29:51 +0200

    fix name

--- task_2.py
+++ task_2.py
@@ -1,14 +1,13 @@
 import random
 
 
-def get_numbers_ticet(min: int, max: int, quantity: int) -> list:
+def get_numbers_ticket(min: int, max: int, quantity: int) -> list:
     if min >= 1 and max <= 1000 and quantity <= max:
         return sorted(random.sample(range(min, max + 1), quantity))
-    else:
-        return []
+    return []
 
 
 if __name__ == "__main__":
-    print(get_numbers_ticet(1, 5, 5))
-    print(get_numbers_ticet(1, 5, 3))
-    print(get_numbers_ticet(1, 5, 23))
+    print(get_numbers_ticket(1, 5, 5))
+    print(get_numbers_ticket(1, 5, 3))
+    print(get_numbers_ticket(1, 5, 23))
