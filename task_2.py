70627ec75dbae89b8d34efd6f4f7d6eacf934aa6
alex <alex.kudelko@gmail.com>
2025-03-09 19:31:06 +0200

    fix if

--- task_2.py
+++ task_2.py
@@ -2,12 +2,14 @@ import random
 
 
 def get_numbers_ticket(min: int, max: int, quantity: int) -> list:
-    if min >= 1 and max <= 1000 and quantity <= max:
+    if 1 <= min <= max <= 1000 and quantity <= (max - min + 1):
         return sorted(random.sample(range(min, max + 1), quantity))
     return []
 
 
 if __name__ == "__main__":
-    print(get_numbers_ticket(1, 5, 5))
     print(get_numbers_ticket(1, 5, 3))
+    print(get_numbers_ticket(1, 5, 5))
     print(get_numbers_ticket(1, 5, 23))
+    print(get_numbers_ticket(100, 5, 5))
+    print(get_numbers_ticket(10, 15, 10))
